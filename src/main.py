b81f9a9ba64d92c5b2e41cc80e5452beea583687
FedrianzD <fedrianzd@gmail.com>
2024-03-13 23:59:11 +0700

    .

--- src/main.py
+++ src/main.py
@@ -86,3 +86,21 @@ sol = []
 sol = bejir(point1, point2, point3, 1, 4)
 sol2 = bejir2(arr, 1, 3)
 print(sol2)
+
+if __name__ == "__main__":
+    point1 = (1,0)
+    point2 = (3,4)
+    point3 = (5,0)
+    sol = []
+    sol = bejir(point1, point2, point3, 1, 3)
+    print(sol)
+    plt.plot([point[0] for point in sol], [point[1] for point in sol], 'ro-')
+    plt.plot([point[0] for point in sol], [point[1] for point in sol], 'b-', label='Kurva Graf Bezier dengan Algoritma Titik Tengah')
+    plt.plot(point1[0], point1[1], 'go')
+    plt.plot(point2[0], point2[1], 'go')
+    plt.plot(point3[0], point3[1], 'go')
+    plt.plot([1, 3], [0, 4], 'r-')
+    plt.plot([3, 5], [4, 0], 'r')
+    plt.title('Kurva Graf Bezier dengan Algoritma Titik Tengah')
+    plt.legend()
+    plt.show()
