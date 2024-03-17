e47c9ab238027b3becb91a0d9cdce7ddb1365c86
bagas <bagassambega@gmail.com>
2024-03-17 21:10:12 +0700

    feat: three point input in gui

--- src/main.py
+++ src/main.py
@@ -1,6 +1,12 @@
 import matplotlib.pyplot as plt
 import function
-import gui
+from gui import Gui
+
+if __name__ == "__main__":
+    App = Gui()
+    App.mainloop()
+
+
 # point1 = (0,0)
 # point2 = (3,3)
 # point3 = (6,0)
@@ -20,19 +26,31 @@ import gui
 # print()
 # print(sol3)
 # print(function.mid((4,1.125), (5.125,1)))
-point1 = (1,0)
-point2 = (3,4)
-point3 = (5,0)
-sol = []
-sol = function.bejir(point1, point2, point3, 1, 5)
-print(sol)
-plt.plot([point[0] for point in sol[0]], [point[1] for point in sol[0]], 'ro-')
-plt.plot([point[0] for point in sol[0]], [point[1] for point in sol[0]], 'b-', label='Kurva Graf Bezier dengan Algoritma Titik Tengah')
-plt.plot(point1[0], point1[1], 'go')
-plt.plot(point2[0], point2[1], 'go')
-plt.plot(point3[0], point3[1], 'go')
-plt.plot([1, 3], [0, 4], 'r-')
-plt.plot([3, 5], [4, 0], 'r')
-plt.title('Kurva Graf Bezier dengan Algoritma Titik Tengah')
-plt.legend()
-plt.show()
+# point1 = (1,0)
+# point2 = (3,4)
+# point3 = (5,0)
+# sol, titikBantu = function.Bezier3Point(point1, point2, point3, 1, 3)
+# print(len(titikBantu))
+# print(sol)
+# for arr in titikBantu:
+#     print(len(arr), arr)
+# for helper_set in titikBantu:
+#         if isinstance(helper_set[0], list):  # multiple helper points
+#             for helper_points in helper_set:
+#                 points_x = [point[0] for point in helper_points]
+#                 points_y = [point[1] for point in helper_points]
+#                 plt.plot(points_x, points_y, linestyle='dotted', color='green')
+#         else:  # single helper point
+#             points_x = [point[0] for point in helper_set]
+#             points_y = [point[1] for point in helper_set]
+#             plt.plot(points_x, points_y, linestyle='dotted', color='green')
+# plt.plot([point[0] for point in sol], [point[1] for point in sol], 'ro')
+# plt.plot([point[0] for point in sol], [point[1] for point in sol], 'b-', label='Kurva Graf Bezier dengan Algoritma Titik Tengah')
+# plt.plot(point1[0], point1[1], 'go')
+# plt.plot(point2[0], point2[1], 'go')
+# plt.plot(point3[0], point3[1], 'go')
+# plt.plot([1, 3], [0, 4], 'r-')
+# plt.plot([3, 5], [4, 0], 'r')
+# plt.title('Kurva Graf Bezier dengan Algoritma Titik Tengah')
+# plt.legend()
+# plt.show()
